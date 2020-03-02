3572f2b19fa80f08d29fa4cf20bcbb5897bdd2fd
pari <pari@PARI-VAIO>
2020-03-02 21:22:45 +0330

    Add Unit OF WORK AND Customer Repository

---
+++ Accounting.DataLayer/Repositories/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Accounting.DataLayer.Context;
+using System.Collections.Generic;
+
+namespace Accounting.DataLayer.Repositories
+{
+    public interface ICustomerRepository
+    {
+        List<Customers> GettAll();
+        Customers GetById(int id);
+        void InsertCustomers(Customers customer);
+        void Update(Customers customer);
+        void Delete(Customers customer);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,60 @@
+﻿using Accounting.DataLayer.Context;
+using Accounting.DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Services
+{
+    class CustomerRepository : ICustomerRepository
+    {
+        private Accounting_DBEntities db;
+
+        public CustomerRepository(Accounting_DBEntities context)
+        {
+            db = context;
+        }
+
+        public List<Customers> GettAll()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetById(int id)
+        {
+            return db.Customers.Find(id);
+        }
+
+        public void InsertCustomers(Customers customer)
+        {
+            db.Customers.Add(customer);
+        }
+
+
+        public void Update(Customers customer)
+        {
+            db.Entry(customer).State = EntityState.Modified;
+        }
+
+        public void Delete(Customers customer)
+        {
+            db.Entry(customer).State = EntityState.Deleted;
+        }
+
+        public void Delete(int id)
+        {
+            var customer = GetById(id);
+            Delete(customer);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+
+    }
+}
